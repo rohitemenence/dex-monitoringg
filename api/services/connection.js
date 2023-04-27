209c6967b1110ad4ea5a179f8497be22656c7793
Rajat Jain <rajatjain.eminence@gmail.com>
2023-04-27 15:29:38 +0530

    calculated token market data

--- api/services/connection.js
+++
@@ -1,6 +0,0 @@
-const ethers = require("ethers");
-
-const connection = "https://mainnet.infura.io/v3/e5e1f07bbce3486598aeee1d006b7c91";
-// console.log('connection', connection)
-
-module.exports = new ethers.providers.JsonRpcProvider(connection);
